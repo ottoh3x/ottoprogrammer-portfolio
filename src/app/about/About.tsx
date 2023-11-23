5855568c1710d1b50ad0b273c8d9846cccb328fb
Othmane Khess <otmankhess@gmail.com>
2023-11-23 01:46:57 +0100

    first

--- src/app/about/About.tsx
+++ src/app/about/About.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Image from "next/image";
 
@@ -13,29 +14,11 @@ import {
 } from "react-icons/si";
 
 
-export const textVariant2 = {
-  hidden: {
-    opacity: 0,
-    y: 20,
-  },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      type: 'tween',
-      ease: 'easeIn',
-      duration:0.6,
-      delay:0.4
-    },
-  },
-};
+
 function About() {
   return (
       <div className="flex flex-col justify-center relative  mx-auto container h-full p-6  my-6 space-y-6 rounded-lg shadow-lg">
         <div 
-        variants={textVariant2}
-        initial="hidden"
-        whileInView="show"
         className="flex flex-col items-center p-4 mt-10">
           {" "}
           <h1 
@@ -44,8 +27,8 @@ function About() {
           </h1>
         </div>
         <div className="flex flex-col gap-4">
-          <img whileHover={{rotate:50}} className="object-contain mx-auto max-w-max w-60  rounded-full" src="/pfp.png" />
-        <p initial={{opacity:0}} whileInView={{opacity:1,transition:{duration:0.6,delay:0.25}}} className="text-gray-400 text-xl text-center font-custom2 font-extralight leading-relaxed
+          <img  className="object-contain mx-auto max-w-max w-60  rounded-full" src="/pfp.png" />
+        <p  className="text-gray-400 text-xl text-center font-custom2 font-extralight leading-relaxed
  md:w-1/2 mx-auto">
         Hi i'm OTTO Programmer.I'm a Freelance Full Stack Devoloper, and I'm very passionate and dedicated to my work.With 3 years experience as a professional Full Stack Devoloper. I have acquired the skills necessary to build great and premium websites.
         </p></div>
