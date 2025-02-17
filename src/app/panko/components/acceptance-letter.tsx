d49838503aab22a27f77b95df692cb8d3b4d15a7
Otto Programmer <70418756+ottoh3x@users.noreply.github.com>
2025-02-17 10:59:46 +0200

    Update acceptance-letter.tsx

--- src/app/panko/components/acceptance-letter.tsx
+++ src/app/panko/components/acceptance-letter.tsx
@@ -13,7 +13,7 @@ const programs = [
   { value: "International Business", label: "International Business" },
   { value: "Business Management", label: "Business Management" },
   { value: "Dental Hygiene", label: "Dental Hygiene" },
-  { value: "Development and Maintenance of Information Systems", label: "Development and Maintenance of Information Systems" }
+  { value: "Development and Maintenance of Information Systems", label: "Development and Maintenance of Information Systems" },
   { value: "Electronic Engineering and Robotics", label: "Electronic Engineering and Robotics" }
 ];
 
